5512e451052cf7820f1a52f778c45dca2816e317
macrandrea <58860052+macrandrea@users.noreply.github.com>
2021-12-01 15:12:55 +0100

    Add files via upload

--- Assignment.m
+++ Assignment.m
@@ -1,12 +1,9 @@
 clc; 
 clear;
-%A=randi(100, [3,3]);
-%A=[4,3;6,3];
-%[L,U, g,ba]=lufact(A);
-%[Elle,Uu,Gi, BaE]=
+MinEntry=0;
 MaxEntry=10; MaxRow=4; MaxCols=4;
-[Gi, BaE]=testamatrici(10, 4, 4);
-%statisticheLU(MaxEntry, MaxRow, MaxCols);
+[Gi, BaE]=testamatrici(MaxEntry, MaxRow, MaxCols);
+%statistiche
 mediumError =mean    (BaE);
 varError    =var     (BaE);
 minError    =min     (BaE);
@@ -15,3 +12,8 @@ mediumGrowth=mean    (Gi);
 varGrowth   =var     (Gi);
 minGrowth   =min     (Gi);
 maxGrowth   =max     (Gi);
+%statisticheLU(MaxEntry, MaxRow, MaxCols);
+%A=randi(100, [3,3]);
+%A=[4,3;6,3];
+%[L,U, g,ba]=lufact(A);
+%[Elle,Uu,Gi, BaE]=
