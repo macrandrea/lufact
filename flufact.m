0774647cc9730627f9f17f53b38cfca722fb40f8
PasqM <pasqmigl97@gmail.com>
2021-12-03 11:02:00 +0100

    Correzioni alla funzione lufact + aggiunta di una versione un po' più veloce (flufact)

--- flufact.m
+++ flufact.m
@@ -1,13 +1,13 @@
-function [L,U] = lufact(A)
-U=A;
+function [L,U] = flufact(A)
+U=transpose(A);
 n=length(U);
 m=width(U);
 if m==n % check for square
     for k=1:n-1 %colfor j=k+1:n %row
         if U(k,k)~=0 %check if we are dividing by 0
             for j=k+1:n
-                U(j,k)=U(j,k)/U(k,k);
-                U(j,k+1:n)=U(j,k+1:n)-U(j,k)*U(k,k+1:n);
+                U(k,j)=U(k,j)/U(k,k);
+                U(k+1:n,j)=U(k+1:n,j)-U(k,j)*U(k+1:n,k);
             end
         else
             error('factorization breaks down due to division by a quantity smaller than machine precision')
@@ -16,6 +16,7 @@ if m==n % check for square
 else
      error('non-square matrix')
 end
+U=transpose(U);
 L=tril(U,-1)+eye(n,n);
 U=triu(U);
 g=makeG(A,L,U); %growth factor
