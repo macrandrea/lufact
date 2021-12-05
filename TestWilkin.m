94a2674d9f8deba28a087990d79f08c7696e48d8
macrandrea <58860052+macrandrea@users.noreply.github.com>
2021-12-05 13:02:36 +0100

    Add files via upload

--- TestWilkin.m
+++ TestWilkin.m
@@ -1,14 +1,10 @@
 clear;
 clc;
-%n=2;
-%A=wilkin(n);
-%e=ones(1,n)';
-%b=A*e;
-%x=A\e;
+n=2;
+A=wilkin(n);
+e=ones(1,n)';
+b=A*e;
+x=A\e;
 %if n=1 then GEPP is accurate with a W
-%H=hilb(10);
-%[L,U]=flufact(H);
-A=randi(100, [10,10]);
-A = A(randperm(size(A, 1)), :); 
-%A = makeDD(A);
-[L,U]=flufact(A);
+H=hilb(10);
+[L,U]=flufact(H);
