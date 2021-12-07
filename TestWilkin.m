40697cd3c3fcf63601f7057809926d0f2fa30a80
macrandrea <58860052+macrandrea@users.noreply.github.com>
2021-12-08 00:28:27 +0100

    FINALE
    FINALE

--- TestWilkin.m
+++ TestWilkin.m
@@ -1,10 +1,12 @@
 clear;
 clc;
-n=60;
+n=55;%54;
 A=wilkin(n);
 e=ones(1,n)';
 b=A*e;
-x=A\e;
+x=A\b;
 %if n=1 then GEPP is accurate with a W
 %H=hilb(10);
-[L,U]=flufact(A);
+[L,U]=lufact(A);
+ba=makeBa(A,L,U);
+g=makeG(A,L,U);
